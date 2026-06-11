1c84f8394da1e8061624200cb2f014f489817e4e
Kishan Atada <katada@student.cscc.edu>
2026-06-10 22:46:47 -0400

    Add sandwich loop exercise in the PA1 file

---
+++ PA1.py
@@ -0,0 +1,8 @@
+sandwich_order = ['Tortas', 'panini', 'Grilled cheese', 'Cuban']
+finished_sandwiches = []
+
+for i in sandwich_order:
+    print(f"I made your {i} sandwich")
+    finished_sandwiches.append(i)
+    
+print(finished_sandwiches)
